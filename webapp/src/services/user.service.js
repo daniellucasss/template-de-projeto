bddc2327fee69950ccaf8d0eefebd4c9d3fc7b81
yrachid <anuar.yasser@gmail.com>
2020-03-11 18:55:39 -0300

    Substitui vue por boilerplate do create-react-app

--- webapp/src/services/user.service.js
+++
@@ -1,8 +0,0 @@
-import http from '../utils/http'
-
-export default {
-    save: (user) => http.post(`/user/`, user)
-        .then(response => alert('salvo com sucesso'))
-        .catch(error => alert('ocorreu um erro'))
-
-}
