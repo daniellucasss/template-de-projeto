384e22533f8d0618c9d7f969358049a3ef483252
Jean Sarlôn <jean.sarlon@icloud.com>
2020-04-20 20:54:27 -0300

    feature: cria componente header

---
+++ frontend/src/components/header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { string, element } from 'prop-types';
+import './header.scss';
+import Link from '../link';
+
+const linkProps = {
+  href: 'https://reactjs.org',
+  target: '_blank',
+  rel: 'noopener noreferrer',
+  className: 'App-link'
+};
+
+const Header = ({ title, className }) => (
+  <header className={className}>
+    {title}
+    <Link {...linkProps}>Learn React</Link>
+  </header>
+);
+
+Header.propTypes = {
+  title: string.isRequired,
+  className: string.isRequired,
+  children: element
+};
+
+export default Header;
