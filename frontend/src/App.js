6e329b427b03ddcafbdcc6e0749a3ba8556bc1e4
Jean Sarlôn <jean.sarlon@icloud.com>
2020-04-23 13:17:43 -0300

    adiciona diretorio services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header';
-import encodingService from './encoding-service';
+import encodingService from './services/encoding-service';
 
 function App() {
   const [encoding, setEncoding] = useState({
